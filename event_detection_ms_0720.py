801fab39ecbc55a240e8b14477a4faaa860d408e
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2021-07-20 12:39:43 +0900

    Update and rename event_detection_ms.py to event_detection_ms_0720.py

--- event_detection_ms_0720.py
+++ event_detection_ms_0720.py
@@ -11,6 +11,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def mslinear_regression(x,y):
+    x = np.array(x); y = np.array(y); 
+    x = x[np.isnan(x)==0]; y = y[np.isnan(y)==0]
+    
+    n = x.shape[0]
+    r = (1/(n-1)) * np.sum(((x - np.mean(x))/np.std(x)) * ((y - np.mean(y))/np.std(y)))
+    m = r*(np.std(y)/np.std(x))
+    b = np.mean(y) - np.mean(x)*m
+
+    return m, b # bx+a
+
 path = 'C:\\mass_save\\itch\\s1214-1\\'
 fname = 'Results_5HT_before_1229.csv'
 
@@ -76,7 +87,7 @@ for se in range(len(mouselist)):
                     peak_sw = True
             peaks_ix = np.array(peaks_ix)
         
-            figsavename = 'C:\\mass_save\\itch\\figsave\\method3\\' + 'se_'+str(se) + '_ws_'+str(ws) + '_ROI_'+str(ROI)
+            figsavename = 'C:\\mass_save\\itch\\figsave\\method2\\' + 'se_'+str(se) + '_ws_'+str(ws) + '_ROI_'+str(ROI)
             if False:
                 plt.figure(); plt.title('all_peak_' + mouselist[se] + '_ws_'+str(ws) + '_ROI_'+str(ROI))
                 plt.plot(msout); plt.scatter(peaks_ix, msout[peaks_ix], c='r'); plt.plot(np.ones(len(msout)) * cut) 
@@ -108,7 +119,33 @@ for se in range(len(mouselist)):
                 plt.savefig(figsavename + '_method1.png', dpi=100); plt.close()
                 
             # method2, 첫 paek이후 reset 조건
-            
+            peaks_ix3 = np.array(peaks_ix)
+            onsw = True
+            reset_thr = 0.5
+            saved = []; t = 0
+
+            for t2 in range(len(msout)):
+                if t2 < t: continue
+                if onsw and t2 in peaks_ix3:
+                    onsw = False
+                    saved.append(t2)
+                    
+                    highpeaks = [t2]; onsw2 = False; accu = 0
+                    for t in range(t2+1, len(msout)):
+                        accu += 1
+                        for t3 in [highpeaks[0]]:
+                            if msout[t3]*reset_thr > msout[t]: onsw2 = True
+                                
+                        if onsw2: onsw = True; break
+                        if t in peaks_ix3: highpeaks.append(t) # peak 갱신
+                
+            if True:
+                plt.figure(); plt.title('method2_' + mouselist[se] + '_ws_'+str(ws) + '_ROI_'+str(ROI))
+                plt.plot(msout); plt.scatter(saved, msout[saved], c='r'); plt.plot(np.ones(len(msout)) * cut)
+                plt.savefig(figsavename + '_method2.png', dpi=500); plt.close()
+        
+                
+            # method3, 2에서 개량, 모든 peak 대상으로 점검, 시간이후 자동 reset
             peaks_ix3 = np.array(peaks_ix)
             onsw = True
             reset_thr = 0.5
@@ -130,11 +167,43 @@ for se in range(len(mouselist)):
                         if t in peaks_ix3: highpeaks.append(t) # peak 갱신
                 
             if True:
-                plt.figure(); plt.title('method2_' + mouselist[se] + '_ws_'+str(ws) + '_ROI_'+str(ROI))
+                plt.figure(); plt.title('method3_' + mouselist[se] + '_ws_'+str(ws) + '_ROI_'+str(ROI))
                 plt.plot(msout); plt.scatter(saved, msout[saved], c='r'); plt.plot(np.ones(len(msout)) * cut)
-                plt.savefig(figsavename + '_method2.png', dpi=500); plt.close()
+                plt.savefig(figsavename + '_method3.png', dpi=500); plt.close()
             
+            # method4, 3에서 개량, 하락중이면 peak 무효, 
+            peaks_ix3 = np.array(peaks_ix)
+            onsw = True
+            reset_thr = 0.35
+            saved = []; t = 0
+
+            for t2 in range(len(msout)):
+                if t2 < t: continue
+                if onsw and t2 in peaks_ix3:
+                    onsw = False
+                    saved.append(t2)
+                    
+                    highpeaks = [t2]; onsw2 = False; accu = 0
+                    for t in range(t2+1, len(msout)):
+                        accu += 1
+                        for t3 in highpeaks:
+                            if msout[t3]*reset_thr > msout[t]: onsw2 = True
+                                
+                        if onsw2 or accu > 10: onsw = True; break
+                        if t in peaks_ix3: highpeaks.append(t) # peak 갱신
             
+            saved2 = []
+            for peak in saved:
+                cluster = msout[np.max([peak-7, 0]):np.min([peak+2, len(msout)])] 
+                x = range(len(cluster))
+                y = cluster
+                m, b = mslinear_regression(x,y)
+                if m > 0: saved2.append(peak)
+ 
+            if True:
+                plt.figure(); plt.title('method4_' + mouselist[se] + '_ws_'+str(ws) + '_ROI_'+str(ROI))
+                plt.plot(msout); plt.scatter(saved2, msout[saved2], c='r'); plt.plot(np.ones(len(msout)) * cut)
+                plt.savefig(figsavename + '_method4.png', dpi=500); plt.close()
 
             
             
