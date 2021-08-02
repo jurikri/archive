e8bd7c02523aea17bf7675c7902c464c67fe315a
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2021-08-02 10:53:12 +0900

    Update auto_turboreg_run.py

--- auto_turboreg_run/auto_turboreg_run.py
+++ auto_turboreg_run/auto_turboreg_run.py
@@ -2,13 +2,13 @@
 """
 Created on Thu Jul 29 14:05:21 2021
 
-@author: MSBak
+ref@author: MSBak
 """
 
 
 #%% oir -> tif
 import os
-path = 'C:\\mass_save\\auto_turboreg_run\\work2'
+path = 'C:\\mass_save\\auto_turboreg_run\\work3'
 
 # must follow these rules!
 # 1. C: <- should be typed mannualy. Do not copy and paste!
@@ -30,7 +30,7 @@ for i in range(len(file_list)):
 with open(path + '\\hello.txt', 'w') as file:    # hello.txt 파일을 쓰기 모드(w)로 열기
     file.writelines(macro_list)
 
-
+import sys; sys.exit()
 
 
 #%% turboreg and save
@@ -41,10 +41,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# import javabridge
-# import bioformats # pip install python-bioformats
-# javabridge.start_vm(class_path=bioformats.JARS)
-
 file_list = os.listdir(path)
 listsave = []
 for i in range(len(file_list)):
@@ -52,44 +48,102 @@ for i in range(len(file_list)):
         filepath = path + '\\' + file_list[i]
         listsave.append(filepath) 
 print('total sample #', len(listsave))
-    
+
+i  = 0
 for i in range(len(listsave)):       
     filepath = listsave[i]
     print(i, '/', len(listsave), '>>>', filepath)
     img0 = io.imread(filepath) # 3 dimensions : frames x width x height
     # print(np.mean(np.std(img0, axis=0)))
-
     sr = StackReg(StackReg.RIGID_BODY)
-
     # register to mean image
     start = time.time()  
     out_mean = sr.register_transform_stack(img0, reference='mean')
-    print("time :", time.time() - start, 's') 
-    # print(np.mean(np.std(out_mean, axis=0)))
-    out_mean2 = np.array(out_mean, dtype='uint16')
-    imageio.mimwrite(filepath + '_reg.tif', out_mean2)
-    print('save as', filepath + '_reg.tif')
+    print("time :", time.time() - start) 
+
+    out_mean2 = np.array(out_mean, dtype='uint32')
+    savename = filepath + '_python_reg32.tif'
+    imageio.mimwrite(savename, out_mean2)
+    print('save as', savename)
     
+    # out_mean2 = np.array(out_mean, dtype='uint16')
+    # savename = filepath + '_python_reg16.tif'
+    # imageio.mimwrite(savename, out_mean2)
+    # print('save as', savename)
     
 
 #%% validation
-if True:
-    filepath = 'C:\\mass_save\\auto_turboreg_run\\' + 'Image5_Sham_D10_M10_reg.tif'
+if False:
+    filepath = 'C:\\mass_save\\auto_turboreg_run\\work2\\' + 'D12.oir_ij_reg.tif'
     img1 = io.imread(filepath) # 3 dimensions : frames x width x height
-    print(np.mean(np.std(img1, axis=0)))
+
+    # filepath = 'C:\\mass_save\\auto_turboreg_run\\work2\\' + 'D12.oir.tif_python_reg.tif'
+    # out_mean_valid = io.imread(filepath) # 3 dimensions : frames x width x height
     
-    plt.figure()
-    plt.plot(np.std(img0, axis=0).flatten(),alpha=0.7, label='original')
-    plt.plot(np.std(img1, axis=0).flatten(),alpha=0.5, label='image J')
-    plt.legend()
+    for f in range(len(out_mean2.shape)):
+        np.sum(np.abs(out_mean2[f,:,:] - img1[f,:,:]) > 1)
     
-    filepath = 'C:\\mass_save\\auto_turboreg_run\\work\\' + 'Image5_Sham_D10_M10.oir.tif_reg.tif'
-    out_mean = io.imread(filepath) # 3 dimensions : frames x width x height
     
-    plt.figure()
-    plt.plot(np.std(out_mean, axis=0).flatten(),alpha=0.7, label='python')
-    plt.plot(np.std(img1, axis=0).flatten(),alpha=0.5, label='image J')
-    plt.legend()
+#%% validation2
+if False:
+    import pandas as pd
+    
+    loadpath = 'C:\\mass_save\\auto_turboreg_run\\work3\\' + 'ij.csv'
+    ij = np.array(pd.read_csv(loadpath))[:,1:]
+    print(ij.shape)
+    
+    loadpath = 'C:\\mass_save\\auto_turboreg_run\\work3\\' + 'python16.csv'
+    python16 = np.array(pd.read_csv(loadpath))[:,1:]
+    print(python16.shape)
+    
+    loadpath = 'C:\\mass_save\\auto_turboreg_run\\work3\\' + 'python32.csv'
+    python32 = np.array(pd.read_csv(loadpath))[:,1:]
+    print(python32.shape)
+    
+    
+    for ROI in range(0, 10, 1):
+        plt.figure()
+        plt.plot(ij[:,ROI], label='ij')
+        plt.plot(python16[:,ROI], label='ij')
+        plt.plot(python32[:,ROI], label='ij')
+        plt.legend()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
