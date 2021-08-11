df5c36be89e013ab4fd713a447b6f193e2f53ded
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2021-08-11 11:26:15 +0900

    Update and rename event_detection_ms_0720.py to ms_event_detection.py

--- ms_event_detection.py
+++ ms_event_detection.py
@@ -48,6 +48,7 @@ for se in range(len(mouselist)):
     matrix = np.array(df)[:,1:]
     roinum = matrix.shape[1]
     allo = np.zeros(matrix.shape) * np.nan
+    allo2 = np.zeros(matrix.shape) * np.nan
     
     cut = 0.3
     
@@ -57,10 +58,9 @@ for se in range(len(mouselist)):
         base = np.sort(matrix[:,ROI])[0:int(round(matrix.shape[0]*baseratio))]
         mean = np.mean(base)
         deltaF = (matrix[:,ROI] - mean) / mean
-        
-        deltaF2 = ms_smooth(mssignal=deltaF, ws=ws)
-        
-        allo[:,ROI] = deltaF
+
+        allo[:,ROI] = ms_smooth(mssignal=deltaF, ws=ws)
+        allo2[:,ROI] = ms_smooth(mssignal=deltaF, ws=2)
     
     # allo는 따로 하드에 저장
     
@@ -105,18 +105,22 @@ for se in range(len(mouselist)):
             
         # 제외 2 - peak 시점으로 부터 상승 - 하강이 아니면 제외. regression slope 으로 판정
 
-        l1, l2 = 5, 2
+        l1, l2 = 4, 0
         
         # p = peaks_ix2[0]
         peaks_ix3 = []
         for p in peaks_ix2:
-            cluster_before = mssignal[np.max([p-l1, 0]):np.min([p+l2, len(mssignal)])] 
-            x = range(len(cluster_before)) ; y = cluster_before
-            m_before, b = mslinear_regression(x,y)
+            cluster_before = mssignal[np.max([p-l1, 0]):np.min([p+l2, len(mssignal)])]
+            if len(cluster_before) > 1:
+                x = range(len(cluster_before)) ; y = cluster_before
+                m_before, b = mslinear_regression(x,y)
+            else: m_before = 1
             
-            cluster_after = mssignal[np.max([p-l2, 0]):np.min([p+l1, len(mssignal)])] 
-            x = range(len(cluster_after)) ; y = cluster_after
-            m_after, b = mslinear_regression(x,y)
+            cluster_after = mssignal[np.max([p-l2, 0]):np.min([p+l1, len(mssignal)])]
+            if len(cluster_after) > 1:
+                x = range(len(cluster_after)) ; y = cluster_after
+                m_after, b = mslinear_regression(x,y)
+            else: m_after = -1
             
             if m_before > 0 and m_after < 0: peaks_ix3.append(p)
         if False: 
@@ -124,16 +128,20 @@ for se in range(len(mouselist)):
 
         
         # 제외 3 - 앞 peak 들의 max 로부터 35% 이하의 감소가 없고, 10 frame 이내면 제외
+        # peak 제거시 update 되도록 수정해야함
         p = 1
-        peaks_ix4 = []
-        if len(peaks_ix3) > 0:
-            peaks_ix4 = [peaks_ix3[0]]
-            for p in range(1, len(peaks_ix3)):
-                c1 = mssignal[peaks_ix3[p-1]] * (1-0.35) > np.min(mssignal[peaks_ix3[p-1]:peaks_ix3[p]])
-                c2 = peaks_ix3[p] - peaks_ix3[p-1] > 10
-                if c1 or c2: peaks_ix4.append(peaks_ix3[p])
-            if False: 
-                plt.figure(); plt.plot(mssignal); plt.scatter(peaks_ix4, mssignal[peaks_ix4], c='r'); plt.plot(np.ones(len(mssignal)) * cut) 
+        peaks_ix4 = list(peaks_ix3)
+        for epoch in range(int(1e6)):
+            passsw = True
+            if len(peaks_ix4) > 0:
+                for p in range(1, len(peaks_ix4)):
+                    c1 = mssignal[peaks_ix4[p-1]] * (1-0.35) > np.min(mssignal[peaks_ix4[p-1]:peaks_ix4[p]])
+                    c2 = peaks_ix4[p] - peaks_ix4[p-1] > 10
+                    if not(c1 or c2): peaks_ix4.pop(p); passsw = False; break
+            if passsw: break
+                
+        if False: 
+            plt.figure(); plt.plot(mssignal); plt.scatter(peaks_ix4, mssignal[peaks_ix4], c='r'); plt.plot(np.ones(len(mssignal)) * cut) 
             
         if True:
             figsavename = 'A:\\itch_event_detection\\figsave\\0811\\' + 'se_'+str(se) + '_ws_'+str(ws) + '_ROI_'+str(ROI)
