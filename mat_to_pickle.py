731e9f93a1c046d9d3693ef865e54d2a1be14992
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2022-05-03 15:33:26 +0900

    Update mat_to_pickle.py

--- mat_to_pickle.py
+++ mat_to_pickle.py
@@ -1,25 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 15:59:25 2022
-
-@author: PC
-"""
 
 import hdf5storage
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import pickle
 
-loadpath = 'C:\\Users\\PC\\Downloads\\s0518-2_5HT_1-3.avi.mat'
-df = hdf5storage.loadmat(loadpath)
-diffplot = df['msdiff_gauss']
+path1 = 'C:\\Users\\MSBak\\Downloads\\behavior-20220502T064636Z-001\\behavior\\'
 
+file_list = os.listdir(path1)
 
-filepath = ''
-filename = ''
+for fname in file_list:
+    path2 = path1 + fname + '\\'
+    file_list2 = os.listdir(path2)
+    
+    for i in range(len(file_list2)):
+        if file_list2[i][-4:] == '.mat':
+            df = hdf5storage.loadmat(path2 + file_list2[i])
+            diffplot = df['msdiff_gauss']
+            mssave = diffplot[0,:]
 
+            savename = path2 + file_list2[i][:-7] + 'pickle'
+            with open(savename, 'wb') as f:  # Python 3: open(..., 'wb')
+                pickle.dump(mssave, f, pickle.HIGHEST_PROTOCOL)
+                print(savename, '저장되었습니다.') 
+                
+#%%
+path1 = 'C:\\Users\\MSBak\\Downloads\\behavior-20220502T064636Z-001\\behavior\\s0518-2\\'
+file_list = os.listdir(path1)
+for i in range(len(file_list)):
+    if file_list[i][-7:] == '.pickle':
+        print(file_list[i])
 
-savename = filepath + filename + '.pickle'
-mssave = diffplot[0,:]
-with open(savename, 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump(mssave, f, pickle.HIGHEST_PROTOCOL)
-    print(savename, '저장되었습니다.') 
+    with open(path1 + file_list[i], 'rb') as f:  # Python 3: open(..., 'rb')
+        repeat_save = pickle.load(f)
+        
+    plt.figure()
+    plt.plot(repeat_save)
+    
+    import sys; sys.exit()
+        
